1b1baa17349d5d13d91c5b15e0bfa7ef3155a12a
Marek Pagel <pagel.marek@gmail.com>
2013-10-16 19:59:41 +0300

    lisasin temppici ja tegin introt ümber

--- src/ee/ut/math/tvt/unum_quadrigis/IntroUI.java
+++ src/ee/ut/math/tvt/unum_quadrigis/IntroUI.java
@@ -1,50 +1,73 @@
 package ee.ut.math.tvt.unum_quadrigis;
 
-import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridLayout;
-import java.awt.Toolkit;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class IntroUI {
 	JFrame raam;
-	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize(); //ekraani suuruse ja laiuse jaoks
-	int x,y; 
-	
+	JPanel raam_ülemine;
+	JPanel raam_logo;
+	// Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+	// //ekraani suuruse ja laiuse jaoks
+	int x, y;
+
 	public IntroUI() {
 		super();
-		
-		x = (int) (screenSize.getWidth()*0.25);
-		y = (int) (screenSize.getHeight()*0.5);
+
+		x = 400;
+		y = 600;
 		raam = new JFrame();
 		raam.setSize(x, y);
 		raam.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		raam.setVisible(true);
-		raam.setLayout(new GridLayout(6,1));
-		raam.setLocationRelativeTo(null);	
+
+		raam.setLayout(new GridLayout(2, 1));
+		raam.setLocationRelativeTo(null);
 		raam.setTitle("Unum Quagridis");
-		JPanel teamName=new JPanel(),leaderName=new JPanel(),leaderEmail=new JPanel(),members=new JPanel(),logo=new JPanel(),version=new JPanel();
-		Font f = new Font(Font.SERIF,1,20),g=new Font(Font.SANS_SERIF,0,16);
-		JLabel tn=new JLabel("Unum Quadrigis"),ln=new JLabel("Marek Pagel"),le=new JLabel("pagel.marek@gmail.com"),m=new JLabel("Marek Pagel, Oskar Hint, Eerik Muuli, Janar Ojalaid"),lg=new JLabel("tiimi logo"),v=new JLabel("versioon");
-		tn.setFont(f);ln.setFont(g);le.setFont(g);m.setFont(g);lg.setFont(g);v.setFont(g);
-		teamName.add(tn);
-		leaderName.add(ln);
-		leaderEmail.add(le);
-		members.add(m);
-		logo.add(lg);
-		version.add(v);
-		raam.add(teamName);
-		raam.add(leaderName);
-		raam.add(leaderEmail);
-		raam.add(members);
-		raam.add(logo);
-		raam.add(version);
-		
+		raam_ülemine = new JPanel();
+		raam_ülemine.setLayout(new GridLayout(5, 1));
+
+		raam.add(raam_ülemine);
+		raam_logo = new JPanel();
+		raam.add(raam_logo);
+//		JPanel teamName = new JPanel(), leaderName = new JPanel(), leaderEmail = new JPanel(), members = new JPanel(), logo = new JPanel(), version = new JPanel();
+		Font f = new Font(Font.SERIF, 1, 20), g = new Font(Font.SANS_SERIF, 0,
+				16);
+
+		JLabel tn = new JLabel("Unum Quadrigis", JLabel.CENTER), ln = new JLabel("Marek Pagel", JLabel.CENTER), le = new JLabel(
+				"pagel.marek@gmail.com", JLabel.CENTER), m = new JLabel(
+				"Marek Pagel, Oskar Hint, Eerik Muuli, Janar Ojalaid", JLabel.CENTER), lg = new JLabel(
+				new ImageIcon("lib/temppic.jpg"), JLabel.CENTER), v = new JLabel("versioon", JLabel.CENTER);
+		tn.setFont(f);
+		ln.setFont(g);
+		le.setFont(g);
+		m.setFont(g);
+		lg.setFont(g);
+		v.setFont(g);
+		// teamName.add(tn);
+		// leaderName.add(ln);
+		// leaderEmail.add(le);
+		// members.add(m);
+		// logo.add(lg);
+		// version.add(v);
+		raam_ülemine.add(tn);
+		raam_ülemine.add(ln);
+		raam_ülemine.add(le);
+		raam_ülemine.add(m);
+		raam_logo.add(lg);
+		raam_ülemine.add(v);
+		// raam_ülemine.add(teamName);
+		// raam_ülemine.add(leaderName);
+		// raam_ülemine.add(leaderEmail);
+		// raam_ülemine.add(members);
+		// raam_logo.add(logo);
+		// raam_ülemine.add(version);
+		raam.setVisible(true);
+
 	}
-	
-	
 
 }
